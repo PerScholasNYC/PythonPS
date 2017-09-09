a587085eda1a7520aafb27fb178739088d757609
PerScholasNYC <kumargoodwinekennedygmail.com>
2017-09-09 15:32:26 -0700

    Homework 2 Part B

---
+++ HW2B.py
@@ -0,0 +1,12 @@
+if 9 % 2 > 0:
+    print ("9: is an odd number")
+else:
+    print ("9: is an even number")
+if 19 % 2 > 0:
+    print ("19: is an odd number")
+else:
+    print ("19: is an even number")
+    if 20 % 2 > 0:
+        print ("20: is an odd number")
+    else:
+        print ("20: is an even number")
