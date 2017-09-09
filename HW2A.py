99181b5d989aa75d978954095f1f7c1cbbe70f71
PerScholasNYC <kumargoodwinekennedygmail.com>
2017-09-09 15:28:53 -0700

    Homework 2 Part A

---
+++ HW2A.py
@@ -0,0 +1,8 @@
+# Find the average of the numbers
+Answer1 = (39 + 45 + 55 + 90 + 95 + 96)/6
+Answer2 = (54-45-10+90)/4
+Answer3 = (55+65+75+95+32)/5
+
+print("The average of 39, 45, 55, 90, 95, and 96 is:", Answer1)
+print("The average of 54, -45, -10, and 90 is:", Answer2)
+print("The average of 55, 65, 75, 95, and 32 is:", Answer3)
