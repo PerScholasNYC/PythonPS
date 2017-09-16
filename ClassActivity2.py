ac1447c9bdc802af8ac27627a471c8fc39cdef4c
PerScholasNYC <kumargoodwinekennedygmail.com>
2017-09-16 16:00:49 -0700

    Class assignments

---
+++ ClassActivity2.py
@@ -0,0 +1,35 @@
+"""x = int(input("Is your number divisble by by 4? >"))
+
+if x % 4 == 0:
+    print("Yes")
+
+else:
+    print("No")"""
+
+"""print("Type a number that is from 6-12 or 121-151")
+n = int(input(">"))
+if 6 < n < 12:
+    print("Your answer is between 6-12")
+
+elif 121 < n < 151:
+    print("You answer is between 121 - 151")"""
+
+"""def print_one():
+    print("arg 1 is greater")
+
+def print_two():
+    print("arg 2 is greater")
+
+def Movies(horror, action):
+    if horror > action:
+        print(horror / action)
+        print_one()
+    elif action > horror:
+        print(horror * 10)
+        print_two()
+    else:
+        print("both are same")
+
+Movies(5, 9)
+Movies(2, 5)
+Movies(20, 10)"""
