e10e7d860d1b2bf3f77191fc2ea0f247ac7492d5
PerScholasNYC <kumargoodwinekennedy@gmail.com>
2017-09-23 06:56:40 -0700

    Class assignmemts 9/21/17

---
+++ Classwork.py
@@ -0,0 +1,59 @@
+"""print("What did you make on your test?")
+x = int(input())
+
+if 90 <= x <= 100:
+    print("You got an A")
+
+elif 80 <= x <= 90:
+    print("You got a B")
+
+elif 70 <= x <= 80:
+    print("You got a C")
+
+elif 55 <= x <= 70:
+    print("You got a D")
+
+elif 55 <= x <= 0:
+    print("You got a F")
+
+else:
+    print("Error: Input must be an integer 100 or less.")"""
+
+##55 TO 15
+"""for x in range(5, 16, 3):
+    print('inside the for loop')
+    print(x)
+    print(9 + 11)
+    print(x + 1)"""
+
+'''for x in range(3, 91, 3):
+    print(x)'''
+
+'''word = 'python class'
+for letter in word:
+    #print(letter)
+    print("hello")
+    '''
+'''i = 0
+while (i<5):
+    print(i)
+    i = i +1'''
+
+'''my_list = [1,2,3,4,5]
+print(my_list)'''
+
+'''numbers = []
+print(numbers)
+for i in range(1, 11):
+    numbers.append(i)
+
+print(numbers)
+print(len(numbers))
+print(numbers[4])'''
+
+
+multiples = []
+for i in range(12, 49, 2):
+    multiples.append(i)
+
+print(multiples)
