9a02c5444feca1cbf871a665d7c84bf0b604c4e8
PerScholasNYC <kumargoodwinekennedy@gmail.com>
2017-11-01 18:26:30 -0700

    Website working

--- webapp/Portfolio/portfolio/views.py
+++ webapp/Portfolio/portfolio/views.py
@@ -3,20 +3,21 @@ from . import app
 from flask import render_template, request
 ## CREATE AN INDEX ROUTE AND A FUNCTION(PAGE) FOR ITd
 
-comments = [{input() :input()}]
+comments = [{'name' : 'kumar', 'message':'hi'}]
 
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('/index.html')
 
 @app.route('/ITCareer', methods = ['GET', 'POST'])
 def ITCareer():
-	if request.method == 'POST':
-		name1 = request.form['User']
-		message1 = request.form['THoughts']
-		comments.append({'name' : name1, 'message': message1)
-
-		print(comments)
-		return render_template('/ITCareer.html', messages = comments)
+    if request.method == 'POST':
+        name1 = request.form['User']
+        message1 = request.form['Thoughts']
+        comments.append({'name' : name1, 'message': message1})
+        print(comments)
+        return render_template('/ITCareer.html', messages = comments)
+    elif request.method == 'GET':
+        return render_template('/ITCareer.html', messages = comments)
