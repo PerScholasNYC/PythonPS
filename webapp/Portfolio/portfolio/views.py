7595f0723e6c54aa2e0582f2c015bf14222a43ad
PerScholasNYC <kumargoodwinekennedy@gmail.com>
2017-11-15 22:54:31 -0800

    Flask Site Complete

--- webapp/Portfolio/portfolio/views.py
+++ webapp/Portfolio/portfolio/views.py
@@ -3,7 +3,7 @@ from . import app
 from flask import render_template, request
 ## CREATE AN INDEX ROUTE AND A FUNCTION(PAGE) FOR ITd
 
-comments = [{'name' : 'kumar', 'message':'hi'}]
+comments = [{'name' : 'Kumar', 'message':'Cool'}]
 
 
 
@@ -11,13 +11,42 @@ comments = [{'name' : 'kumar', 'message':'hi'}]
 def index():
     return render_template('/index.html')
 
-@app.route('/ITCareer', methods = ['GET', 'POST'])
-def ITCareer():
-    if request.method == 'POST':
-        name1 = request.form['User']
-        message1 = request.form['Thoughts']
-        comments.append({'name' : name1, 'message': message1})
-        print(comments)
-        return render_template('/ITCareer.html', messages = comments)
-    elif request.method == 'GET':
-        return render_template('/ITCareer.html', messages = comments)
+@app.route('/Naruto', methods = ['GET', 'POST'])
+def Naruto():
+        if request.method == 'POST':
+            name1 = request.form['User']
+            message1 = request.form['Thoughts']
+            comments.append({'name' : name1, 'message': message1})
+            print(comments)
+            return render_template('/Comments''.html', messages = comments)
+        elif request.method == 'GET':
+            return render_template('/Naruto.html', messages = comments)
+        return render_template('/Naruto.html')
+
+@app.route('/Luffy', methods = ['GET', 'POST'])
+def Luffy():
+        if request.method == 'POST':
+            name1 = request.form['User']
+            message1 = request.form['Thoughts']
+            comments.append({'name' : name1, 'message': message1})
+            print(comments)
+            return render_template('/Comments.html', messages = comments)
+        elif request.method == 'GET':
+            return render_template('/Luffy.html', messages = comments)
+        return render_template('/Luffy.html')
+
+@app.route('/Ichigo', methods = ['GET', 'POST'])
+def Ichigo():
+        if request.method == 'POST':
+            name1 = request.form['User']
+            message1 = request.form['Thoughts']
+            comments.append({'name' : name1, 'message': message1})
+            print(comments)
+            return render_template('/Comments.html', messages = comments)
+        elif request.method == 'GET':
+            return render_template('/Ichigo.html', messages = comments)
+        return render_template('/Ichigo.html')
+
+@app.route('/Comments', methods = ['GET', 'POST'])
+def Comments():
+        return render_template('/Comments.html')
